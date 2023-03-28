c089e534296369b80612b5c94c6867a1048d8d37
Xaver Schüll <botacode@gmail.com>
2023-03-28 20:37:14 +0200

    Added condition and temperature display

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,12 @@
+import "./List.css";
+
 export default function List({ activities, headline }) {
   return (
-    <ul>
+    <ul className="activities">
       <h2>{headline}</h2>
-      <aside>You should do some:</aside>
+      <aside>You should:</aside>
       {activities.map((activity) => (
-        <li key={activity.id}>
+        <li key={activity.id} className="activity">
           <p>{activity.name}</p>
         </li>
       ))}
